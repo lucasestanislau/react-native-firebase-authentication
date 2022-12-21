2e8c3ad4033cc759aa543276858307679b3b7a90
Lucas Vidoto <lucasvidoto17x@gmail.com>
2022-12-20 21:37:40 -0300

    feat: show current email user

--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import Cabecalho from '../../componentes/Cabecalho';
-import Produto from '../../componentes/Produtos';
-import estilos from './estilos';
+import React from "react";
+import { View, Text } from "react-native";
+import Cabecalho from "../../componentes/Cabecalho";
+import Produto from "../../componentes/Produtos";
+import estilos from "./estilos";
+import { auth } from "../../config/firebase";
 
 export default function Principal({ navigation }) {
+  const usuario = auth.currentUser;
 
   return (
     <View style={estilos.container}>
       <Cabecalho navigation={navigation} />
-      <Text style={estilos.texto}>Usuário: teste@email.com</Text>
+      <Text style={estilos.texto}>Usuário: {usuario.email}</Text>
 
       <Produto nome="Tênis" preco="200,00" />
       <Produto nome="Camisa" preco="100,00" />
       <Produto nome="Suplementos" preco="150,00" />
-     </View>
+    </View>
   );
 }
