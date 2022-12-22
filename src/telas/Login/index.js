367ffa9095ce2a6b24d80030f9bee5441c77626b
Lucas Vidoto <lucasvidoto17x@gmail.com>
2022-12-21 23:15:41 -0300

    feat: loading animation

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import Botao from "../../componentes/Botao";
 import { EntradaTexto } from "../../componentes/EntradaTexto";
 import { logar } from "../../servicos/requisicoesFirebase";
 import estilos from "./estilos";
 import { auth } from "../../config/firebase";
+import animacaoCarregando from "../../../assets/animacaoCarregando.gif";
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+
+  const [carregando, setCarregando] = useState(true);
 
   useEffect(() => {
     const estadoUsuario = auth.onAuthStateChanged((usuario) => {
       if (usuario) {
         navigation.replace("Principal");
       }
+      setCarregando(false);
     });
 
     return () => estadoUsuario();
@@ -23,7 +27,17 @@ export default function Login({ navigation }) {
   async function realizarLogin() {
     const resultado = await logar(email, senha);
 
-    navigation.replace("Principal");
+    if (resultado) {
+      navigation.replace("Principal");
+    }
+  }
+
+  if (carregando) {
+    return (
+      <View style={estilos.containerAnimacao}>
+        <Image source={animacaoCarregando} style={estilos.imagem} />
+      </View>
+    );
   }
 
   return (
